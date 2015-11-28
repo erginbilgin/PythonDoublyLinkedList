e3c42f8356a0250a8334da20d8c3e60a7ec97bbc
Ergin Bilgin <erginbil@gmail.com>
2015-11-29 00:25:51 +0200

    implement removeItem()

--- MyLinkedList.py
+++ MyLinkedList.py
@@ -1,45 +1,64 @@
 from Node import Node
 
 class MyLinkedList(object):
-	count = 0
-	head = None
-	tail = None
-	#def __init__(self):
-	def isEmpty(self):
-		return self.count == 0
-	def addItem(self, value):
-		node = Node(value)
-		if self.isEmpty():
-			self.head = node
-			self.tail = node
-		else:
-			node.prev = self.tail
-			self.tail.next = node
-			self.tail = node
-		self.count += 1
-	def insertItem(self, value, position):
-		node = Node(value)
-		if self.count > position:
-			if position == 0:
-				node.next = self.head
-				self.head.prev = node
-				self.head = node
-			else:
-				currentNode = self.head
-				for i in range(0, position):
-					currentNode = currentNode.next
-				node.next = currentNode
-				node.prev = currentNode.prev
-				currentNode.prev.next = node
-				currentNode.prev = node
-			self.count += 1
-	def printList(self):
-		output = '['
-		currentNode = self.head
-		while currentNode is not None:
-			output += str(currentNode.value)
-			currentNode = currentNode.next
-			if currentNode is not None:
-				output += ', '
-		output += ']'
-		print(output)
+    count = 0
+    head = None
+    tail = None
+    #def __init__(self):
+    def isEmpty(self):
+        return self.count == 0
+    def addItem(self, value):
+        node = Node(value)
+        if self.isEmpty():
+            self.head = node
+            self.tail = node
+        else:
+            node.prev = self.tail
+            self.tail.next = node
+            self.tail = node
+        self.count += 1
+    def insertItem(self, value, position):
+        node = Node(value)
+        if self.count > position:
+            if position == 0:
+                node.next = self.head
+                self.head.prev = node
+                self.head = node
+            else:
+                currentNode = self.head
+                for i in range(0, position):
+                    currentNode = currentNode.next
+                node.next = currentNode
+                node.prev = currentNode.prev
+                currentNode.prev.next = node
+                currentNode.prev = node
+            self.count += 1
+    def removeItem(self, position):
+        if self.count > position:
+            if self.count != 1:
+                if position == 0:
+                    self.head.next.prev = head.prev
+                    self.head = head.next
+                if position == self.count-1:
+                    self.tail.prev.next = tail.next
+                    self.tail = tail.prev
+                if position > 0 and position < self.count-1:
+                    currentNode = self.head
+                    for i in range(0, position):
+                        currentNode = currentNode.next
+                    currentNode.next.prev = currentNode.prev
+                    currentNode.prev.next = currentNode.next
+            else:
+                    head = Node()
+                    tail = Node()
+            self.count -= 1
+    def printList(self):
+        output = '['
+        currentNode = self.head
+        while currentNode is not None:
+            output += str(currentNode.value)
+            currentNode = currentNode.next
+            if currentNode is not None:
+                output += ', '
+        output += ']'
+        print(output)
--- main.py
+++ main.py
@@ -8,6 +8,13 @@ list.addItem(9)
 list.addItem(13)
 list.addItem(23)
 list.addItem(231)
+
+list.printList()
+
 list.insertItem(99, 5)
+
+list.printList()
+
+list.removeItem(5)
 
 list.printList()
